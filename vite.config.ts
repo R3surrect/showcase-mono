6e5cae134d5b828866d19d42ad6d55e1bf68fda5
R3surrect <stixway@gmail.com>
2026-05-18 18:56:56 +0500

    storybook: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   server: {
-    host: true,
+    host: true
   },
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ['react-compiler']
-        ]
-      }
-    }),
-  ],
+  plugins: [react({
+    babel: {
+      plugins: [['react-compiler']]
+    }
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -27,7 +30,7 @@ export default defineConfig({
       '@store': path.resolve(__dirname, './src/store'),
       '@validation': path.resolve(__dirname, './src/validation'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      "@routes/*": path.resolve(__dirname, "./src/routes"),
+      "@routes/*": path.resolve(__dirname, "./src/routes")
     }
   },
   build: {
@@ -35,5 +38,27 @@ export default defineConfig({
     modulePreload: {
       polyfill: true
     }
+  },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
   }
-})
+});
