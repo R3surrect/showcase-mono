b019da56f130e169be8da101be8ccf5f65ca6393
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-05-25 19:50:53 +0500

    feat(pages): wrap tag creation inputs into a responsive grid layout

--- src/pages/Templates/Tags/Tags.tsx
+++ src/pages/Templates/Tags/Tags.tsx
@@ -6,6 +6,7 @@ import Input from '@components/ui/Input/Input'
 import { ColorPicker } from '@components/ui/ColorPicker/ColorPicker'
 import Stack from '@components/ui/Stack/Stack'
 import { Tag } from '@components/ui/Tag/Tag'
+import Grid from '@/components/ui/Grid/Grid'
 
 // TODO Отработать ситуацию с легкой тенью текста и внутренней тени,
 // TODO чтобы если юзер решил создать тег под цвет фона - все равно было видно
@@ -38,7 +39,10 @@ export const Component = () => {
         <Surface>
             <Stack gap='md'>
                 <Heading level={5} variant='secondary'>Создать тег</Heading>
-                <Input labelText='Название' placeholder='Новый тег' type='text' />
+                <Grid columns={2} >
+                    <Input labelText='Название' placeholder='Новый тег' type='text' />
+                    <Input labelText='Эмодзи' placeholder='🏷️' type='text' />
+                </Grid>
                 <ColorPicker />
             </Stack>
         </Surface>
