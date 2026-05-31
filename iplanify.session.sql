da3f4a99769ac6367e0e0fd2552df487aadfd047
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-05-31 21:47:25 +0500

    postgresql init
    typescript init

---
+++ iplanify.session.sql
@@ -0,0 +1,64 @@
+CREATE TYPE task_status_enum AS ENUM (
+    "completed" | "pending" | "overdue" | "scheduled" | 'in_progress'
+);
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username varchar(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash varchar(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+);
+CREATE TABLE IF NOT EXISTS tags (
+    id SERIAL PRIMARY KEY,
+    label VARCHAR(50) NOT NULL,
+    color VARCHAR(50) NOT NULL,
+    owner_id INT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+
+    CONSTRAINT fk_tags_owner
+        FOREIGN KEY (owner_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title varchar(50) not null,
+    details TEXT,
+    status task_status_enum DEFAULT 'scheduled' NOT NULL,
+    deadline TIMESTAMPTZ,
+    notify_at TIMESTAMPTZ,
+    owner_id INT NOT NULL,
+    tags VARCHAR(50)[] DEFAULT '{}' NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMETIMESTAMPTZ DEFAULT NOW() NOT NULL,
+
+    CONSTRAINT fk_tasks_owner
+        FOREIGN KEY (owner_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS tasks_tags_pivot (
+    task_id INT NOT NULL,
+    tag_id INT NOT NULL,
+
+    PRIMARY KEY (task_id, tag_id),
+
+    CONSTRAINT fk_pivot_task
+        FOREIGN KEY (task_id)
+        REFERENCES tasks(id)
+        ON DELETE CASCADE
+
+    CONSTRAINT fk_pivot_tag
+        FOREIGN KEY (tag_id)
+        REFERENCES tags(id)
+        ON DELETE CASCADE
+)
+CREATE TABLE IF NOT EXISTS notes (
+    id SERIAL PRIMARY KEY,
+    
+)
+
+INSERT INTO projects (title, details, owner_id) 
+VALUES ('life', 'Твой дефолтный проект для повседневных задач', user_id);
