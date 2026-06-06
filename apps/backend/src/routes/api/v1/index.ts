14dfce0702a4cdd39745eea19579c3b3cb85c5fb
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-06-06 21:19:27 +0500

    feat: connect auth guard to app

--- apps/backend/src/routes/api/v1/index.ts
+++ apps/backend/src/routes/api/v1/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import registerRouter from "./auth/register/register.js";
 import loginRouter from "./auth/login/login.js";
+import { authMiddleware } from "#/middleware/auth.js";
 
 const v1Router = new Hono();
 
 v1Router.route("/auth", registerRouter);
 v1Router.route("/auth", loginRouter);
+
+v1Router.use('/*', authMiddleware)
 
 export default v1Router;
