1f78424e734ffef6830bf51c5d594754f9c166f1
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-06-07 20:10:18 +0500

    fix(api-client): options parameters implement

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,4 +1,4 @@
 import type { AppRouterV1 } from '@showcase-mono/backend/src/routes/api/v1/index'
 import { hc } from 'hono/client';
 
-export const createApiClient = (baseUrl: string) => hc<AppRouterV1>(`${baseUrl}/api/v1`);
+export const createApiClient = (baseUrl: string, options?: Parameters<typeof hc>[1]) => hc<AppRouterV1>(`${baseUrl}/api/v1`, options);
