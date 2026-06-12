f2bd4940a67e39894627f7f9f9051da5bd725d3f
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-06-12 16:01:18 +0500

    refactor(frontend): integrate unified backend tag types and update Tag component
    - Imported TagClientPayload from backend to enforce strict end-to-end data contracts.
    - Updated TagProps by omitting conflicting HTML attributes (color, id) and merging with backend payload.
    - Refactored <Tag /> component to support HSL color objects and handled string conversion for HTML id.
    - Updated tag list rendering to pass down full item props and fixed key mappings.
    - Added .vscode to .gitignore.

--- packages/frontend/src/components/entities/Tag/Tag.tsx
+++ packages/frontend/src/components/entities/Tag/Tag.tsx
@@ -1,10 +1,11 @@
 import type { ColorVariable, TagProps } from '@/components/entities/Tag/Tag.types';
 import stylesObj from './Tag.module.css';
 
-const Tag = ({ label, emoji, color, ...props }: TagProps) => {
+const Tag = ({ label, emoji, color, id, ...props }: TagProps) => {
     return <div
+        id={id.toString()}
         className={stylesObj.tag}
-        style={{ '--tag-color': `${color}` } as ColorVariable}
+        style={{ '--tag-color': `hsl(${color.h}, ${color.s}%, ${color.l}%)` } as ColorVariable}
         {...props}
     >
         {emoji}
--- packages/frontend/src/components/entities/Tag/Tag.types.ts
+++ packages/frontend/src/components/entities/Tag/Tag.types.ts
@@ -1,8 +1,8 @@
-export interface TagProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'style' | 'className'> {
-    label: string;
-    emoji: string
-    color: string;
-}
+import type { TagClientPayload } from "@showcase-mono/backend/routes/api/v1/templates/tags/tag.types";
+
+export type TagProps = Omit<
+    Partial<React.HTMLAttributes<HTMLDivElement>>, 'style' | 'className' | 'color' | 'id'
+> & TagClientPayload
 
 export interface ColorVariable extends React.CSSProperties {
     '--tag-color': string;
