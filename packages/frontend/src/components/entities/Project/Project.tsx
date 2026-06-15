e8cceab70dbe8ae0fa0da126ec21b61468c0d7b3
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-06-15 20:42:28 +0500

    WIP: feat(progressbar): Progressbar implement

--- packages/frontend/src/components/entities/Project/Project.tsx
+++ packages/frontend/src/components/entities/Project/Project.tsx
@@ -9,7 +9,21 @@ import { DEFAULT_HSL_COLOR } from '@components/entities/_shared/system.constants
 import { emojiToUnified } from '@components/entities/EmojiPicker/EmojiPicker.constants';
 import Text from '@components/entities/Text/Text';
 
+const tasksMock = { completed: 25, pending: 50, overdue: 30, scheduled: 40, inProgress: 15 };
+
+const sortTasks = (tasks) => {
+    const keys = Object.keys(tasks);
+    const sortedTasks = {};
+
+    keys.sort((a, b) => tasks[a] - tasks[b])
+
+    for (const key of keys) {
+        sortedTasks[key] = tasks[key];
+    }
+}
+
 const Project = ({ emoji, color, isPinned = false, label, description }: ProjectProps) => {
+
     return <Surface variant="solid" isAnimated>
         <Stack direction="column" gap="md">
             <Stack direction="row" gap="md" align='center'>
@@ -32,14 +46,13 @@ const Project = ({ emoji, color, isPinned = false, label, description }: Project
                 </Stack>
             </Stack>
             <Text color='lightgray' weight='bold' size={6}>{description}</Text>
-            <progress
-                className={stylesObj.progress}
-                style={{
-                    '--project-color': getHslString(color || DEFAULT_HSL_COLOR)
-                } as ProjectVars}
-                value="70"
-                max="100"
-            />
+            <div className={stylesObj.progress}>
+                <div className={stylesObj.completed} id='completed'></div>
+                <div className={stylesObj.pending} id='pending'></div>
+                <div className={stylesObj.overdue} id='overdue'></div>
+                <div className={stylesObj.scheduled} id='scheduled'></div>
+                <div className={stylesObj.in_progress} id='in_progress'></div>
+            </div>
         </Stack>
     </Surface>
 }
