246f8898d313e63ff24e2e5dbc05aa973774ce6c
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-06-19 19:07:40 +0500

    feat(projects): include projects router to app routes barrel

--- packages/backend/src/routes/api/v1/index.ts
+++ packages/backend/src/routes/api/v1/index.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "#/middleware/auth.js";
 import templatesRouter from "./templates/index.js";
 import meRouter from "./auth/me/me.js";
 import logoutRouter from "./auth/logout/logout.js";
+import projectsRouter from "./projects/projects.js";
 
 const v1Router = new Hono();
 
@@ -15,8 +16,10 @@ export const appRouterV1 = v1Router
     .route("/auth", loginRouter)
     .route("/auth", meRouter)
     .route("/auth", logoutRouter)
-    .route('/templates', templatesRouter);
-    // .route('/projects', projectsRouter)
+    .route('/templates', templatesRouter)
+    .route('/projects', projectsRouter)
+
+    ;
 
 export type AppRouterV1 = typeof appRouterV1;
 export default v1Router;
