d496183e3606cd49f7efea83c29f1c8b2f21760f
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-06-29 20:00:10 +0500

    WIP: portal slot implement

--- packages/frontend/src/components/entities/ContentHeader/ContentHeader.tsx
+++ packages/frontend/src/components/entities/ContentHeader/ContentHeader.tsx
@@ -1,6 +1,6 @@
 import Heading from '@/components/entities/Heading/Heading.tsx';
 import Stack from '@components/entities/Stack/Stack';
-import stylesObj from './ContentHeader.module.css';
+// import stylesObj from './ContentHeader.module.css';
 import Text from '../Text/Text';
 import type { ContentHeaderProps } from '@/components/entities/ContentHeader/ContentHeader.types';
 
@@ -13,16 +13,16 @@ export const ContentHeader = ({ title, subtitle, children }: ContentHeaderProps)
         {title}
     </Heading>
 
-    return <Stack direction='row' justify='space-between' wrap={true}>
+    return <Stack direction='row' justify='space-between' wrap={true} >
         {subtitle ? <Stack gap='sm' direction='column' width='fit'>
             {heading}
             <Text color='darkgray' size={6} weight='regular'>{subtitle}</Text>
         </Stack>
             : heading
         }
-        <div className={stylesObj.content}>
+        <Stack direction='row' id="content-header-slot">
             {children}
-        </div>
+        </Stack>
     </Stack>
 
 }
