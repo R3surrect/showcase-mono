66515bc2e966d6881020c2a2425503e0b9c308be
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-07-17 18:39:45 +0500

    WIP: toast component implement

--- packages/frontend/src/components/entities/Toast/Toast.tsx
+++ packages/frontend/src/components/entities/Toast/Toast.tsx
@@ -1,10 +1,27 @@
 import { FloatingPortal } from "@floating-ui/react";
 import stylesObj from './Toast.module.css';
+import Stack from "@components/entities/Stack/Stack";
+import Text from "@components/entities/Text/Text";
+import Hr from "../Hr/Hr";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { useState } from "react";
 
 const Toast = () => {
+    const [isClosed, setIsClosed] = useState(false);
+
     return <FloatingPortal>
-        <div className={stylesObj.toastWrapper}>
-            toast
+        <div
+            className={stylesObj.toastWrapper}
+            data-closed={isClosed}
+            onClick={() => setIsClosed(true)}
+        >
+            <Stack gap="sm">
+                <div>
+                    <Text as="h2" weight="bolder">Label</Text>
+                    <Hr variant="accent" thickness="half-medium" opacity={0.1} />
+                </div>
+                <ErrorMessage message="SomeMessage" />
+            </Stack>
         </div>
     </FloatingPortal>
 }
