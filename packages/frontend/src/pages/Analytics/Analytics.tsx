7a8d5608ca769792fb8253b26157024cffb6c81f
R3surrect <32245078+R3surrect@users.noreply.github.com>
2026-08-07 23:35:58 +0500

    minor fix

--- packages/frontend/src/pages/Analytics/Analytics.tsx
+++ packages/frontend/src/pages/Analytics/Analytics.tsx
@@ -43,7 +43,7 @@ export const Component = () => {
 
             <DashboardProjectLoadBlock />
             <DashboardTaskPriorityBlock />
-            \
+
             <DashboardInsightStatsBlock />
         </Stack>
     )
