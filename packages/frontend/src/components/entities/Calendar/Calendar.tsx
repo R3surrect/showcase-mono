d66f0b277488ad5b45b499e2551bd29695a65042
r3surrect <stixway@gmail.com>
2026-09-25 20:46:15 +0500

    WIP: calendar task count

--- packages/frontend/src/components/entities/Calendar/Calendar.tsx
+++ packages/frontend/src/components/entities/Calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import { DayPicker, type DayPickerProps } from "react-day-picker"
+import { DayPicker, type DayButtonProps, type DayPickerProps } from "react-day-picker"
 import 'react-day-picker/style.css';
 import './Calendar.css';
 import Surface from "@components/entities/Surface/Surface";
@@ -11,11 +11,28 @@ const stickyStyles = {
     top: '0',
 } as const;
 
-const Calendar = <T extends DayPickerProps>(props: T) =>
-    <div style={stickyStyles}>
+const CustomDayButton = (dayProps: DayButtonProps) => {
+    const { day, modifiers, ...rest } = dayProps;
+
+    return (
+        <button {...rest} className={`${rest.className || ''} my-custom-day-btn`}>
+            {day.date.getDate()}
+        </button>
+    );
+};
+
+const Calendar = <T extends DayPickerProps>(props: T) => {
+
+    return <div style={stickyStyles}>
         <Surface width="max" height="fit" overflow="visible">
-            <DayPicker style={{ width: '100%' }} {...defaultProps} {...props} />
+            <DayPicker
+                style={{ width: '100%' }}
+                components={{ DayButton: CustomDayButton }}
+                {...defaultProps}
+                {...props}
+            />
         </Surface>
     </div>
+}
 
 export default Calendar;
